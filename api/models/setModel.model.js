5a76717686783ac4dc1cef701c1f31562f5f4837
Micky <miguel.angel.angulo72@gmail.com>
2018-10-20 22:44:14 -0400

    Dynamic event messages.

--- api/models/setModel.model.js
+++ api/models/setModel.model.js
@@ -5,9 +5,8 @@ const moment = require('moment');
 exports.setModel = function (req, res, next) {
   let model = req.body.model;
   if (model === 'pressure' || model === 'temp' || model === 'model1' || model === 'model2') {
-    app.emit('event', {
-      data: model,
-      timestamp: moment().format('YYYY-MM-DD HH:mm:ss')
+    app.emit('changeModel', {
+      data: model
     });
     res.json({status: "success", message: `${model} model setted`});
     return false;
@@ -17,26 +16,26 @@ exports.setModel = function (req, res, next) {
 
 
 exports.events = (req, res, next) => {
-  let status;
-  let messageId = 7029468;
   // SSE Setup
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive',
   });
-  res.write('\n');
 
-
-  app.on('event', data => {
-    status = data;
-    writeEvent();
-  })
-
-  setInterval(writeEvent, 20000);
+  app.on('changeModel', model => {
+    setMessage('changeModel', model.data, res)
+  });
+  setInterval(writeEvent, 1000);
   function writeEvent() {
-    res.write(`id: ${messageId}\n`);
-    res.write(`model: ${JSON.stringify(status)}\n\n`);
+    setMessage('idle', 'This is event idle.', res)
   }
+};
+
+function setMessage(event, data, res) {
+  res.write(
+    `event: ${event}\ndata:${data}`
+  );
+  res.write('\n\n');
 }
 
